d4ea3ee65bd2a7ee25ffd58cda16708a9ea8c829
anderson-robertp <anderon-rp@hotmail.com>
2025-05-05 21:26:38 -0500

    header

---
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rb-recipe-item',
+  standalone: false,
+  templateUrl: './recipe-item.component.html',
+  styleUrl: './recipe-item.component.css'
+})
+export class RecipeItemComponent {
+
+}
---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rb-recipe-list',
+  standalone: false,
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent {
+
+}
